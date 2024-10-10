bd31541eb7366e49fd88d3d33ba1ea1dd5100436
Efremov Aleksandr <efremov_an@astralnalog.ru>
2024-10-10 18:56:44 +0300

    wip: Рефакторинг с учетом подключения нового источника данных

--- migrations/data_base/00006_create_table_wb2cards.sql
+++ migrations/data_base/00006_create_table_wb2cards.sql
@@ -1,36 +1,41 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE shop.wb2cards (
+CREATE TABLE shop.seller2cards (
     id serial NOT NULL,
     nmID integer NOT NULL,
     "int" integer NOT NULL,
     nmUUID text NOT NULL,
     created_at timestamp without time zone DEFAULT now(),
     updated_at timestamp without time zone DEFAULT now(),
-    card_id integer NOT NULL
+    card_id integer NOT NULL,
+    seller_id integer NOT NULL
 );
 
-ALTER TABLE shop.wb2cards OWNER TO erp_db_usr;
+ALTER TABLE shop.seller2cards OWNER TO erp_db_usr;
 
-ALTER TABLE ONLY shop.wb2cards
-    ADD CONSTRAINT wb2cards_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY shop.wb2cards
-    ADD CONSTRAINT wb2cards_card_id_fkey FOREIGN KEY (card_id) REFERENCES shop.cards(id);
+ALTER TABLE ONLY shop.seller2cards
+    ADD CONSTRAINT seller2cards_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY shop.seller2cards
+    ADD CONSTRAINT seller2cards_card_id_fkey FOREIGN KEY (card_id) REFERENCES shop.cards(id);
+ALTER TABLE shop.seller2cards
+    ADD CONSTRAINT seller2cards_seller_id_fkey FOREIGN KEY (seller_id) REFERENCES shop.sellers(id);
 
-CREATE INDEX wb2cards_card_id_idx ON shop.wb2cards USING btree (card_id);
+CREATE INDEX seller2cards_card_id_idx ON shop.seller2cards USING btree (card_id);
+CREATE INDEX seller2cards_seller_id_idx ON shop.seller2cards USING btree (seller_id);
 
-COMMENT ON TABLE shop.wb2cards IS 'Товары WB';
-COMMENT ON COLUMN shop.wb2cards.id IS 'Уникальный идентификатор';
-COMMENT ON COLUMN shop.wb2cards.nmID IS 'Артикул WB';
-COMMENT ON COLUMN shop.wb2cards."int" IS 'Идентификатор КТ';
-COMMENT ON COLUMN shop.wb2cards.nmUUID IS 'Внуттренний технический идентификатор товара';
-COMMENT ON COLUMN shop.wb2cards.created_at IS 'Дата создания';
-COMMENT ON COLUMN shop.wb2cards.updated_at IS 'Дата обновления';
-COMMENT ON COLUMN shop.wb2cards.card_id IS 'Идентификатор номенклатуры';
+COMMENT ON TABLE shop.seller2cards IS 'Товары WB';
+COMMENT ON COLUMN shop.seller2cards.id IS 'Уникальный идентификатор';
+COMMENT ON COLUMN shop.seller2cards.nmID IS 'Артикул WB';
+COMMENT ON COLUMN shop.seller2cards."int" IS 'Идентификатор КТ';
+COMMENT ON COLUMN shop.seller2cards.nmUUID IS 'Внуттренний технический идентификатор товара';
+COMMENT ON COLUMN shop.seller2cards.created_at IS 'Дата создания';
+COMMENT ON COLUMN shop.seller2cards.updated_at IS 'Дата обновления';
+COMMENT ON COLUMN shop.seller2cards.card_id IS 'Идентификатор номенклатуры';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX wb2cards_card_id_idx;
-DROP TABLE shop.wb2cards;
+DROP INDEX seller2cards_seller_id_idx;
+DROP INDEX seller2cards_card_id_idx;
+DROP TABLE shop.seller2cards;
 -- +goose StatementEnd
